cc01b9dfc90c627fb1dae28703ff43a5670a204c
luke.quinane@gmail.com <luke.quinane@gmail.com>
2009-09-04 22:36:17 +1000

    Added property change notification to scan settings.

--- TwainDotNet/ResolutionSettings.cs
+++ TwainDotNet/ResolutionSettings.cs
@@ -2,20 +2,58 @@
 using System.Collections.Generic;
 using System.Text;
 using TwainDotNet.TwainNative;
+using System.ComponentModel;
 
 namespace TwainDotNet
 {
-    public class ResolutionSettings
+    public class ResolutionSettings : INotifyPropertyChanged
     {
+        int? _dpi;
+
         /// <summary>
         /// The DPI to scan at. Set to null to use the current default setting.
         /// </summary>
-        public int? Dpi { get; set; }
+        public int? Dpi
+        {
+            get { return _dpi; }
+            set
+            {
+                if (value != _dpi)
+                {
+                    _dpi = value;
+                    OnPropertyChanged("Dpi");
+                }
+            }
+        }
+
+        ColourSetting _colourSettings;
 
         /// <summary>
         /// The colour settings to use.
         /// </summary>
-        public ColourSetting ColourSetting { get; set; }
+        public ColourSetting ColourSetting
+        {
+            get { return _colourSettings; }
+            set
+            {
+                if (value != _colourSettings)
+                {
+                    _colourSettings = value;
+                    OnPropertyChanged("ColourSetting");
+                }
+            }
+        }
+        
+        #region INotifyPropertyChanged Members
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #endregion
 
         /// <summary>
         /// Fax quality resolution.
--- TwainDotNet/ScanSettings.cs
+++ TwainDotNet/ScanSettings.cs
@@ -1,45 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace TwainDotNet
 {
-    public class ScanSettings
+    public class ScanSettings : INotifyPropertyChanged
     {
         public ScanSettings()
         {
-            TransferCount = 1;
+            ShoulTransferAllPages = true;
         }
+
+        bool _showTwainUI;
 
         /// <summary>
         /// Indicates if the TWAIN/driver user interface should be used to pick the scan settings.
         /// </summary>
-        public bool ShowTwainUI { get; set; }
+        public bool ShowTwainUI
+        {
+            get { return _showTwainUI; }
+            set
+            {
+                if (value != _showTwainUI)
+                {
+                    _showTwainUI = value;
+                    OnPropertyChanged("ShowTwainUI");
+                }
+            }
+        }
+
+        bool _useDocumentFeeder;
 
         /// <summary>
         /// Indicates if the automatic document feeder (ADF) should be the source of the document(s) to scan.
         /// </summary>
-        public bool UseDocumentFeeder { get; set; }
+        public bool UseDocumentFeeder
+        {
+            get { return _useDocumentFeeder; }
+            set
+            {
+                if (value != _useDocumentFeeder)
+                {
+                    _useDocumentFeeder = value;
+                    OnPropertyChanged("UseDocumentFeeder");
+                }
+            }
+        }
+
+        short _transferCount;
 
         /// <summary>
         /// The number of pages to transfer.
         /// </summary>
-        public short TransferCount { get; set; }
+        public short TransferCount
+        {
+            get { return _transferCount; }
+            set
+            {
+                if (value != _transferCount)
+                {
+                    _transferCount = value;
+                    OnPropertyChanged("TransferCount");
+                    OnPropertyChanged("ShoulTransferAllPages");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Indicates if all pages should be transferred.
+        /// </summary>
+        public bool ShoulTransferAllPages
+        {
+            get { return _transferCount == TransferAllPages; }
+            set { TransferCount = value ? TransferAllPages : (short) 1; }
+        }
+
+        ResolutionSettings _resolution;
 
         /// <summary>
         /// The resolution settings. Set null to use current defaults.
         /// </summary>
-        public ResolutionSettings Resolution { get; set; }
+        public ResolutionSettings Resolution
+        {
+            get { return _resolution; }
+            set
+            {
+                if (value != _resolution)
+                {
+                    _resolution = value;
+                    OnPropertyChanged("Resolution");
+                }
+            }
+        }
 
-        /// <summary>
-        /// The value to set to scan all available pages.
-        /// </summary>
-        public const short TransferAllPages = -1;
+        bool _useDuplex;
 
         /// <summary>
         /// Whether to use duplexing, if supported.
         /// </summary>
-        public bool UseDuplex { get; set; }
+        public bool UseDuplex
+        {
+            get { return _useDuplex; }
+            set
+            {
+                if (value != _useDuplex)
+                {
+                    _useDuplex = value;
+                    OnPropertyChanged("UseDuplex");
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #endregion
 
         /// <summary>
         /// Default scan settings.
@@ -48,5 +130,10 @@ namespace TwainDotNet
         {
             Resolution = ResolutionSettings.ColourPhotocopier
         };
+
+        /// <summary>
+        /// The value to set to scan all available pages.
+        /// </summary>
+        public const short TransferAllPages = -1;
     }
 }
