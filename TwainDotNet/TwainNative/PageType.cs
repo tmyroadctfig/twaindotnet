aa89d98f21677d00837dbda4a678df915ee165ae
Diego Guidi <diegoguidi@gmail.com>
2010-01-05 09:45:10 +0100

    added PageType enum (unsupported)

---
+++ TwainDotNet/TwainNative/PageType.cs
@@ -0,0 +1,76 @@
+﻿namespace TwainDotNet.TwainNative
+{
+    public enum PageType
+    {
+        None = 0,
+        /// <summary>
+        /// A4 ad A4Letter
+        /// </summary>
+        A4 = 1,
+        /// <summary>
+        /// JISB5 and B5Letter
+        /// </summary>
+        JISB5 = 2,
+        UsLetter = 3,
+        UsLegal = 4,
+        A5 = 5,
+        /// <summary>
+        /// ISOB4 and B4
+        /// </summary>
+        ISOB4 = 6,
+        /// <summary>
+        /// ISOB6 and B6
+        /// </summary>
+        ISOB6 = 7,
+        B = 8,
+        UsLedger = 9,
+        UsExecutive = 10,
+        A3 = 11,
+        /// <summary>
+        /// ISOB3 and B3
+        /// </summary>
+        ISOB3 = 12,
+        A6 = 13,
+        C4 = 14,
+        C5 = 15,
+        C6 = 16,
+        _4A0 = 17,
+        _2A0 = 18,
+        A0 = 19,
+        A1 = 20,
+        A2 = 21,
+        A7 = 22,
+        A8 = 23,
+        A9 = 24,
+        A10 = 25,
+        ISOB0 = 26,
+        ISOB1 = 27,
+        ISOB2 = 28,
+        ISOB5 = 29,
+        ISOB7 = 30,
+        ISOB8 = 31,
+        ISOB9 = 32,
+        ISOB10 = 33,
+        JISB0 = 34,
+        JISB1 = 35,
+        JISB2 = 36,
+        JISB3 = 37,
+        JISB4 = 38,
+        JISB6 = 39,
+        JISB7 = 40,
+        JISB8 = 41,
+        JISB9 = 42,
+        JISB10 = 43,
+        C0 = 44,
+        C1 = 45,
+        C2 = 46,
+        C3 = 47,
+        C7 = 48,
+        C8 = 49,
+        C9 = 50,
+        C10 = 51,
+        UsStatement = 52,
+        BusinessCard = 53,
+        MaxSize = 54,
+    }
+}
