b6f8ea1951da14a2b78b82285666946acc16ccba
David Jeske <davidj@gmail.com>
2017-06-02 15:27:02 -0700

    first working version of incremental scan progress

--- src/TwainDotNet/Win32/BitmapRenderer.cs
+++ src/TwainDotNet/Win32/BitmapRenderer.cs
@@ -106,8 +106,9 @@ namespace TwainDotNet.Win32
             return new Bitmap(imageInfo.ImageWidth,imageInfo.ImageLength,System.Drawing.Imaging.PixelFormat.Format24bppRgb);
         }
 
-        public static void TransferPixels(Bitmap bitmap_dest, ref int pixel_number, TwainDotNet.TwainNative.ImageInfo imageInfo, TwainDotNet.TwainNative.ImageMemXfer memxfer_src)
+        public static void TransferPixels_Test(Bitmap bitmap_dest, ref int pixel_number, TwainDotNet.TwainNative.ImageInfo imageInfo, TwainDotNet.TwainNative.ImageMemXfer memxfer_src)
         {
+            
             int bytes_per_pixel = imageInfo.BitsPerPixel / 8;
             int num_pixels_in_buffer = (int)memxfer_src.BytesWritten / bytes_per_pixel;
 
@@ -120,5 +121,75 @@ namespace TwainDotNet.Win32
                 pixel_number++;
             }
         }
+
+
+
+
+        public static unsafe void TransferPixels(Bitmap bitmap_dest, ref int pixel_number, TwainDotNet.TwainNative.ImageInfo imageInfo, TwainDotNet.TwainNative.ImageMemXfer memxfer_src) {
+            BitmapInfoHeaderStruct bitmapInfo = new BitmapInfoHeaderStruct();
+            bitmapInfo.Width = (int) memxfer_src.Columns;
+            bitmapInfo.Height = - (int) memxfer_src.Rows;
+            bitmapInfo.Size = sizeof(BitmapInfoHeaderStruct);
+            bitmapInfo.BitCount = imageInfo.BitsPerPixel;    
+            bitmapInfo.Planes = 1;
+            bitmapInfo.SizeImage = 0;            
+
+
+            using (Graphics graphics = Graphics.FromImage(bitmap_dest)) {
+                IntPtr hdc = graphics.GetHdc();
+                try {
+                    Gdi32Native.SetDIBitsToDevice(
+                        hdc, 
+                        (int)memxfer_src.XOffset, 
+                        (int)memxfer_src.YOffset, 
+                        (int)memxfer_src.Columns, 
+                        (int)memxfer_src.Rows,
+                        0, 0, 0, 
+                        (int)memxfer_src.Rows,
+                        memxfer_src.Memory.TheMem, 
+                        new IntPtr(&bitmapInfo), 
+                        0);
+                }
+                finally {
+                    graphics.ReleaseHdc(hdc);
+                }
+            }
+
+        }
+
+
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/dd183376(v=vs.85).aspx
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        public struct  BitmapInfoHeaderStruct
+        {
+            public int Size;
+            public int Width;
+            public int Height;
+            public short Planes;
+            public short BitCount;
+            public int Compression;
+            public int SizeImage;
+            public int XPelsPerMeter;
+            public int YPelsPerMeter;
+            public int ClrUsed;
+            public int ClrImportant;
+
+            public override string ToString() {
+                return string.Format(
+                    "s:{0} w:{1} h:{2} p:{3} bc:{4} c:{5} si:{6} xpels:{7} ypels:{8} cu:{9} ci:{10}",
+                    Size,
+                    Width,
+                    Height,
+                    Planes,
+                    BitCount,
+                    Compression,
+                    SizeImage,
+                    XPelsPerMeter,
+                    YPelsPerMeter,
+                    ClrUsed,
+                    ClrImportant);
+            }
+        }
+
     }
 }
